1bb531383f03b792709a8ba7c0eb6a5c80e8a6b4
lucasantos-S <lucassantos.dsilv@gmail.com>
2023-01-18 21:25:56 -0300

    create rep

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import Habit from "./components/Habit";
+
+import "./styles/global.css";
+
+function App() {
+  return <Habit />;
+}
+
+export default App;
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Habit() {
+  return <div></div>;
+}
+
+export default Habit;
