dc81bba9797f4bce023a44147df248b9a1342ced
lucasantos-S <lucassantos.dsilv@gmail.com>
2023-01-26 23:28:20 -0300

    created: completed style

--- web/src/components/SummaryTable/HabitDay.tsx
+++ web/src/components/SummaryTable/HabitDay.tsx
@@ -1,47 +1,43 @@
-import { generateDatesFromYearBeginning } from "../../utils/generate-Dates";
 import * as Popover from "@radix-ui/react-popover";
+import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
 
-const generateDatesFromYear = generateDatesFromYearBeginning();
-const minimunSummaryDatesSize = 18 * 7; // quantidade de quadrados minumos
+interface HabitDayProps {
+  completed: number;
+  amount: number;
+}
 
-const amountOfDatesToFill =
-  minimunSummaryDatesSize - generateDatesFromYear.length;
-export function HabitDay() {
+export function HabitDay(props: HabitDayProps) {
+  const completedPercentage = Math.round(
+    (props.completed / props.amount) * 100
+  );
+
   return (
-    <div className="grid grid-rows-7 grid-flow-col gap-3">
-      {generateDatesFromYear.map((yars, index) => {
-        return (
-          <Popover.Root key={yars.toDateString()}>
-            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></Popover.Trigger>
-            <Popover.Portal>
-              <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-                <span className="font-semibold text-zinc-400">
-                  Segunda Feita
-                </span>
-                <span className=" mt-1 font-extrabold leading-tight text-2xl">
-                  17/01
-                </span>
+    <Popover.Root>
+      <Popover.Trigger
+        className={clsx(
+          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg",
+          {
+            "bg-zinc-900":completedPercentage == 0,
+            "bg-violet-900  border-violet-900":completedPercentage > 0 && completedPercentage < 20,
+            "bg-violet-800  border-violet-800":completedPercentage >= 20 && completedPercentage < 40,
+            "bg-violet-700  border-violet-700":completedPercentage >= 40 && completedPercentage < 60,
+            "bg-violet-600  border-violet-600":completedPercentage >= 60 && completedPercentage < 80,
+            "bg-violet-500  border-violet-500": completedPercentage >= 80,
+          }
+        )}
+      />
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">Segunda Feita</span>
+          <span className=" mt-1 font-extrabold leading-tight text-2xl">
+            17/01
+          </span>
 
-                <ProgressBar progress={75} />
-                <Popover.Arrow
-                  width={20}
-                  height={10}
-                  className=" fill-zinc-900"
-                />
-              </Popover.Content>
-            </Popover.Portal>
-          </Popover.Root>
-        );
-      })}
-
-      {amountOfDatesToFill > 0 &&
-        Array.from({ length: amountOfDatesToFill }).map((_, inedex) => (
-          <div
-            key={inedex}
-            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 opacity-40 cursor-not-allowed rounded-lg"
-          ></div>
-        ))}
-    </div>
+          <ProgressBar progress={75} />
+          <Popover.Arrow width={20} height={10} className=" fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
--- web/src/components/SummaryTable/SummaryTable.tsx
+++ web/src/components/SummaryTable/SummaryTable.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import {HabitDay} from './HabitDay'
-import WeekDays from './WeekDays'
+import { generateDatesFromYearBeginning } from "../../utils/generate-Dates";
+import { HabitDay } from "./HabitDay";
+import WeekDays from "./WeekDays";
+
+const generateDatesFromYear = generateDatesFromYearBeginning();
+const minimunSummaryDatesSize = 18 * 7; // quantidade de quadrados minumos
+
+const amountOfDatesToFill =
+  minimunSummaryDatesSize - generateDatesFromYear.length;
 
 function SummaryTable() {
   return (
     <div className="w-full flex gap-3">
-       <WeekDays/>
-       <HabitDay/>
+      <WeekDays />
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {generateDatesFromYear.map((yars, index) => {
+          console.log(yars);
+          
+          return <HabitDay completed={Math.round(Math.random() * 5)} amount={5}  key={yars.toString()}/>;
+        })}
+
+        {amountOfDatesToFill > 0 &&
+          Array.from({ length: amountOfDatesToFill }).map((_, inedex) => (
+            <div
+              key={inedex}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 opacity-40 cursor-not-allowed rounded-lg"
+            ></div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default SummaryTable
+export default SummaryTable;
